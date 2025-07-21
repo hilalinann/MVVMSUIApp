d3cb1d1369b6dcc445eae164c13693ebe953f8e9
Hilal İnan <hilalinan61@gmail.com>
2025-07-21 21:14:57 +0300

    Initial Commit

---
+++ MVVMSUIApp/MVVMSUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSUIAppApp.swift
+//  MVVMSUIApp
+//
+//  Created by Hilal İnan on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
