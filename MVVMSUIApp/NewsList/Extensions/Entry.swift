78390859a671fe9c8f682244e57b8c8f55b3042b
Hilal İnan <hilalinan61@gmail.com>
2025-07-22 21:18:35 +0300

    entry and extension update

---
+++ MVVMSUIApp/NewsList/Extensions/Entry.swift
@@ -0,0 +1,8 @@
+//
+//  Entry.swift
+//  MVVMSUIApp
+//
+//  Created by Hilal İnan on 22.07.2025.
+//
+
+import Foundation
---
+++ MVVMSUIApp/NewsList/Extensions/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  MVVMSUIApp
+//
+//  Created by Hilal İnan on 22.07.2025.
+//
+
+import Foundation
